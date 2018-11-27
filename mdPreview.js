fd5dca23f362f325d80170ca6fa3a5ec8a599d82
David <davidkueng@gmail.com>
2018-11-27 17:34:23 +0100

    made textarea input work

--- mdPreview.js
+++ mdPreview.js
@@ -1,8 +1,9 @@
 
 
 // document.getElementById('editor').innerHTML = marked();
-
-document.getElementById('editor').onkeyup = console.log(marked('" this is title'))
+let changeContent = () => {
+document.getElementById('editor').onkeyup = console.log(marked('this is title'))
+}
 
 document.getElementById('preview').innerHTML =
     marked('# Marked in browser\n\nRendered by **marked**.');
